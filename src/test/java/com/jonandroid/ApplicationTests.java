1706cd9ba3aec5c53d1cb58750c7ab06da7ccc3c
Manjius <jon.irigoyen.c@gmail.com>
2025-05-02 10:49:52 +0200

    Learning Spring-boots

---
+++ src/test/java/com/jonandroid/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jonandroid;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
